9eaa1ef6411da14906d64903fe15efb23c23ebc1
Kelby Webster <34950917+kelbyweb@users.noreply.github.com>
2019-10-07 20:11:34 -0500

    Update MovieReview.java

--- src/assignment2/MovieReview.java
+++ src/assignment2/MovieReview.java
@@ -1,1 +1,83 @@
+// Kelby Webster and Daniel Sparrow
+// Assignment 2, CS 3354
 
+package assignment2;
+import java.io.Serializable;
+
+
+
+public class MovieReview implements Serializable {
+	
+	public int id;
+	public int realClass;
+	public int predictedPolarity;
+	public String reviewWords;
+	public String filePath;
+	public String firstFiftyChars;
+	
+
+    public MovieReview(String reviewWords, String filePath, int realClass) {
+        this.reviewWords = reviewWords;
+        this.filePath = filePath;
+        this.realClass = realClass;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int i) {
+    	this.id = i;
+    }
+  
+    public String getText() {
+        return this.reviewWords;
+    }
+
+ 
+    public int getPredictedPolarity() {
+        return this.predictedPolarity;
+    }
+
+
+    public void setPredictedPolarity(int predictedPolarity) {
+        this.predictedPolarity = predictedPolarity;
+    }
+
+    
+    public int getRealClass() {
+        return this.realClass;
+    }
+
+    
+    public String getFirstFiftyChars() { 
+    	
+        int fileSize = this.reviewWords.length();
+        String fifty;
+        
+        if(fileSize > 50) {
+            fifty = this.reviewWords.substring(0,50);
+            this.firstFiftyChars = fifty;
+        }
+        else
+            this.firstFiftyChars = this.reviewWords;
+        return this.firstFiftyChars;
+    }
+
+    
+    public int reviewSubstring(String substring) {
+    	
+        int tru = -1;
+        int isTrue;
+        String review = this.reviewWords;
+        tru = review.indexOf(substring);    // returns -1 if not in index, otherwise returns index
+        
+        if(tru != -1) {        
+            isTrue = 1;
+        }
+        else
+            isTrue = 0;
+        
+        return isTrue;
+    }
+}

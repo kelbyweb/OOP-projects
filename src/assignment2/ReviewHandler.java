475c1503901f9b05d498b01bc157b86fc22f4900
Kelby Webster <34950917+kelbyweb@users.noreply.github.com>
2019-10-07 20:21:34 -0500

    Update ReviewHandler.java

--- src/assignment2/ReviewHandler.java
+++ src/assignment2/ReviewHandler.java
@@ -1,1 +1,43 @@
+package assignment2; 
+import java.util.*;
+import java.io.Serializable;
+import java.io.*;
+import java.lang.Math;
+import java.io.File;
+import assignment2.AbstractReviewHandler;
+import assignment2.MovieReview;
+import java.io.FileNotFoundException;
 
+
+public class ReviewHandler extends AbstractReviewHandler {
+  
+    public void loadReviews(String filePath, int realClass) {
+       
+    }
+  
+    public void putInId() {
+       
+    }
+  
+    public MovieReview readReview(String reviewFilePath, int realClass){
+        
+    }
+
+   public void loadSerialDB() {
+       
+    }
+
+    public MovieReview searchById(int id) {
+ 
+        
+    }
+
+    public List<MovieReview> searchBySubstring(String substring) {
+       
+    }
+
+    public void deleteReview(int id) {
+       
+    }
+
+}

f655c26cf57b9459b6a9410244219f140286aa0d
Kelby Webster <kelbyweb@Kelbys-MacBook-Pro.local>
2019-11-07 16:34:25 -0600

    added comments for methods requiring GUI implementation

--- src/assignment3/MainApp.java
+++ src/assignment3/MainApp.java
@@ -18,6 +18,9 @@ import java.awt.event.*;
 
 public class MainApp {
     
+ 	// make review handler object global 
+	private static final ReviewHandler rh = new ReviewHandler();
+	
 	 /**
 		* The main function
 		* @param args to be passed in
@@ -25,39 +28,45 @@ public class MainApp {
 		*/
     public static void main(String [] args) throws IOException {
     	
-    	ReviewHandler rh = new ReviewHandler();
       	
      // first load from serialized file if present
         rh.loadSerialDB();
-        
-        
-     // TODO: Components for GUI layout
-        // All of these are static private final 
-        // top panel (JPanel), bottom panel (JPanel), command label (JLabel), 
-        // combo box (JComboBox), database button (JButton), save button (JButton)
-        
-     // Output area
-        // Set as global so it can be edited in different methods
-        // static protected final output area (JTextArea),
-        // static private final output scroll pane (JScrollPane)
-        
-     // Width & height of the monitor
-        // Both are private static int
-        // width, height  (get default screen size) 
-        
-     // Width & height of the JFrame window
-        // Both are private static int
-        // windows width and height (Both JFrame)
-        
+
+     // call method to create/display GUI
+     // DON'T UNCOMMENT THIS until you're finished with creating panels
+        // createAndDisplayGUI();
     	
-    	// display and loop with choices
+    // display and loop with choices
     	boolean continueMenu = true;
         while(continueMenu == true)
 		{
-			continueMenu = menuOptions(rh);
+			continueMenu = menuOptions();
 		}
              
     }
+    
+    // Components for GUI layout
+    // All of these are global and static private final 
+       static private final JPanel topPanel = new JPanel();
+       static private final JPanel bottomPanel = new JPanel();
+       static private final JLabel commandLabel = new JLabel("Please select your choice :", JLabel.RIGHT);
+       static private final JComboBox comboBox = new JComboBox();
+       static private final JButton dbButton = new JButton("Display Database");
+       static private final JButton saveDbButton = new JButton("Save Database");
+       
+    // Output area
+       static protected final JTextArea outputArea = new JTextArea();
+       static private final JScrollPane outputScrollPane = new JScrollPane(outputArea);
+       
+    // Width & height of the monitor (get default screen size) 
+       private static int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+       private static int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+       
+    // Width & height of the JFrame windows in pixels
+       private static int windowWidth = 800;
+       private static int windowHeight = 600;
+    
+    
    
     /**
      * Initialize the JFrame and JPanels, and show them.
@@ -68,7 +77,17 @@ public class MainApp {
     	createTopPanel();
         createBottomPanel();
     	
-    	
+        // container for top & bottom panel
+        JPanel panelContainer = new JPanel();
+        panelContainer.setLayout(new GridLayout(2,0));
+        panelContainer.add(topPanel);
+        panelContainer.add(bottomPanel);
+        
+        // main window
+        JFrame frame = new JFrame("MainApp");
+        panelContainer.setOpaque(true);
+        frame.setContentPane(panelContainer);
+        frame.setVisible(true);
     	
     }
     
@@ -78,8 +97,14 @@ public class MainApp {
      */
     private static void createTopPanel() {
     	
-    
-    	
+    // each menu option shown as a comboBox.addItem("option text")
+    // add item listener for when an option is selected 
+    // comboBox.AddItemListener(new ItemListener() and then method ItemStateChanged(event e)
+    // add all JLabels to topPanel then call updateUI
+    // if e.getItem().equals("option text") then call appropriate method (firstChoice, secondChoice, etc)
+    // add action listener (with actionPerformed runnable/thread with run method) for database button and save button 
+    // create grid layout and add command label and combo box to it
+    // add all JLabels and buttons to topPanel then call updateUI
     	
     }
     
@@ -91,7 +116,13 @@ public class MainApp {
      */
     private static void createBottomPanel() {
     	
-    	
+    // create new Font (Arial or Times 18 pt) and set to output area
+    // display welcome message with outputArea.setText("Welcome...");
+    // set text in output area as not editable (false)
+    // create black line border then set with outputArea.setBorder
+    // create vertical & horizontal scroll bar for outputScrollPane
+    // set layout of bottomPanel to a new GridLayout(1, 0)
+    // add outputScrollPane to bottomPanel
     	
     }
     
@@ -123,7 +154,7 @@ public class MainApp {
 	 * @throws InputMismatchException if invalid input
 	 * @throws IOException if invalid file input
      */
-	public static boolean menuOptions(ReviewHandler rh) throws IOException
+	public static boolean menuOptions() throws IOException
 	{
 		try
 		{
@@ -149,11 +180,11 @@ public class MainApp {
         	return false;
 		}
 		if(n == 1)
-			firstChoice(rh);
+			firstChoice();
 		if(n == 2)
-			secondChoice(rh);
+			secondChoice();
 		if(n == 3)
-			thirdChoice(rh);
+			thirdChoice();
 		}
 		catch(InputMismatchException x)
 		{
@@ -177,11 +208,8 @@ public class MainApp {
     /**
 	* Takes user input to add a review or directory of reviews to the database.
 	* @throws NumberFormatException if invalid number
-	* @throws InputMismatchException if invalid input
-	* @param rh The ReviewHandler to be passed in
-     * @throws IOException 
 	*/
-	public static void firstChoice(ReviewHandler rh) throws NumberFormatException
+	public static void firstChoice() throws NumberFormatException
 	{
 		try
 		{
@@ -215,11 +243,9 @@ public class MainApp {
     
 	/**
 	* Takes user input to delete a review from the database
-	* @param rh ReviewHandler to be passed in
-	* @throws InputMismatchException if invalid input
 	* @throws IOException if invalid file input
 	*/
-	public static void secondChoice(ReviewHandler rh) throws IOException
+	public static void secondChoice() throws IOException
 	{
 		try
 		{
@@ -238,10 +264,9 @@ public class MainApp {
     
 	/**
 	* Takes user input to search the database by ID or substring
-	* @param rh ReviewHandler to be passed in
 	* @throws InputMismatchException if invalid input
 	*/
-	public static void thirdChoice(ReviewHandler rh)
+	public static void thirdChoice()
 	{
 		 System.out.println("Enter 1 to search by ID or 2 to search by substring :");
     	 Scanner choice = new Scanner(System.in);

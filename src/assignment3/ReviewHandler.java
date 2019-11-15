2922ed379a8fb691fa65cc1b0930f8e55e44d94a
Kelby Webster <websterkelby@gmail.com>
2019-11-14 19:06:07 -0600

    Update ReviewHandler.java

--- src/assignment3/ReviewHandler.java
+++ src/assignment3/ReviewHandler.java
@@ -1,95 +1,101 @@
-package assignment3; 
+package src.assignment3;
 
-import java.io.FileNotFoundException;
-import java.io.BufferedOutputStream;
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.util.HashMap;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import sentiment.Sentiment;
-import java.util.ArrayList;
-import java.io.*;
 
 
 /**
 *
 ** CS3354 Fall 2019 Review Handler Method specification
-   @author Kelby Webster & Daniel Sparrow
+   @author Kelby Webster 
 */
 public class ReviewHandler extends AbstractReviewHandler {
-	
-	/**
-     * Constructor.
-     */
-	public ReviewHandler()
-	 {
-		 super();
-	 }
-	/**
+
+    private static int ID;
+
+    /**
      * Loads reviews from a given path. If the given path is a .txt file, then
      * a single review is loaded. Otherwise, if the path is a folder, all reviews
      * in it are loaded.
      * @param filePath The path to the file (or folder) containing the review(sentimentModel).
      * @param realClass The real class of the review (0 = Negative, 1 = Positive
      * 2 = Unknown).
-     * @return A list of reviews as objects.
+     *
      */
-	public void loadReviews(String filePath, int realClass) {
-		//for menu option 1
-		//calls classifyReview
-		
-		 int key;
-		 File f = new File(filePath);
-		 Scanner inReview;
-		 MovieReview temp;
+    @Override
+    public void loadReviews(String filePath, int realClass) {
+        File fileOrFolder = new File(filePath);
+        try {
+            if (fileOrFolder.isFile()) {
+                // File
+                if (filePath.endsWith(".txt")) {
+                    // Import review
+                    MovieReview review = readReview(filePath, realClass);
+                    // Add to database
+                    database.put(review.getId(), review);
+                    //Output result: single file
+                    SentimentAnalysisApp.outputArea.append("Review imported.");
+                    SentimentAnalysisApp.outputArea.append("ID: " + review.getId());
+                    SentimentAnalysisApp.outputArea.append("Text: " + review.getText());
+                    SentimentAnalysisApp.outputArea.append("Real Class: " + review.getRealPolarity());
+                    SentimentAnalysisApp.outputArea.append("Classification result: " + review.getPredictedPolarity());
+                    if (realClass == 2) {
+                    	SentimentAnalysisApp.outputArea.append("Real class unknown.");
+                    } else if (realClass == review.getPredictedPolarity()) {
+                        SentimentAnalysisApp.outputArea.append("Correctly classified.");
+                    } else {
+                        SentimentAnalysisApp.outputArea.append("Misclassified.");
+                    }
+                    SentimentAnalysisApp.outputArea.append("\n");
 
-		 try
-		 {
-		 if(f.isFile()) //adds singular file to database
-		 {
-			 key = ((int) f.getName().charAt(0)) * f.getName().length() * ( (int) Math.random() * 40 + 1);
-			 inReview = new Scanner(f);
-			 String text = "";
-			 while(inReview.hasNextLine())
-				 text += inReview.nextLine();
-			 temp = new MovieReview(key, text, realClass);
-			 temp.setPredictedPolarity(classifyReview(temp));
-			 database.put(key, temp);
-		 }
-		 else if(f.isDirectory()) //access directory
-		 {
-			 File[] reviewsDir = f.listFiles();
-			 for(File files: reviewsDir)
-			 {
-				key = ((int) files.getName().charAt(0)) * files.getName().length() * ( (int) Math.random() * 40 + 1);
-				inReview = new Scanner(files);
-				String text = "";
-				while(inReview.hasNextLine())
-					text += inReview.nextLine();
-				temp = new MovieReview(key, text, realClass);
-				temp.setPredictedPolarity(classifyReview(temp));
-				database.put(key, temp);
-			 }
-		 }
-		 }
-		 catch(Exception e)
-		 {
-			 System.out.println("Difficulty accessing file/directory.");
-			 System.out.println(e.getMessage());
-		 }
-       
+                } else {
+                    // Cannot import non-txt files
+                    SentimentAnalysisApp.outputArea.append("Input file path is neither a txt file nor folder.");
+                    return;
+                }
+            } else {
+                // Folder
+                String[] files = fileOrFolder.list();
+                String fileSeparatorChar = System.getProperty("file.separator");
+                int counter = 0;
+                for (String fileName : files) {
+                    if (fileName.endsWith(".txt")) {
+                        // Only import txt files
+                        // Import review
+                        MovieReview review = readReview(filePath + fileSeparatorChar + fileName, realClass);
+                        // Add to database
+                        database.put(review.getId(), review);
+                        // Count correct classified reviews, only real class is known
+                        if (realClass != 2 && review.getRealPolarity() == review.getPredictedPolarity()) {
+                            counter++;
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+                // Output result: folder
+                SentimentAnalysisApp.outputArea.append("Folder imported.");
+                SentimentAnalysisApp.outputArea.append("Number of entries: " + files.length);
+
+                // Only output accuracy if real class is known
+                if (realClass != 2) {
+                    SentimentAnalysisApp.outputArea.append("Correctly classified: " + counter);
+                    SentimentAnalysisApp.outputArea.append("Misclassified: " + (files.length - counter));
+                    SentimentAnalysisApp.outputArea.append("Accuracy: " + ((double)counter / (double)files.length * 100) + "%");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+        }
+
     }
-  
-	
-	/**
-     * Reads a single review file and returns it as a MovieReview object. 
+
+    /**
+     * Reads a single review file and returns it as a MovieReview object.
      * This method also calls the method classifyReview to predict the polarity
      * of the review.
      * @param reviewFilePath A path to a .txt file containing a review.
@@ -97,125 +103,123 @@ public class ReviewHandler extends AbstractReviewHandler {
      * @return a MovieReview object.
      * @throws IOException if specified file cannot be openned.
      */
+    @Override
     public MovieReview readReview(String reviewFilePath, int realClass) throws IOException {
-        //for menu options 1 and 3
-        //calls classifyReview, reads single file to add to database or search for substring
-    	 File fi = new File(reviewFilePath);
-		
-		 int key = ((int) reviewFilePath.charAt(0)) * reviewFilePath.length() * ( (int) Math.random() * 40 + 1);
-		 Scanner inReview = new Scanner(fi);
-		 String text = "";
-		 while(inReview.hasNextLine())
-			 text += inReview.nextLine();
-		 MovieReview review = new MovieReview(key, text, realClass);
-		 review.setPredictedPolarity(classifyReview(review));
-		 
-		 return review;
+        // Read file for text
+        Scanner inFile = new Scanner(new FileReader(reviewFilePath));
+        String text = "";
+        while (inFile.hasNextLine()) {
+            text += inFile.nextLine();
+        }
+        // Remove the <br /> occurences in the text and replace them with a space
+        text = text.replaceAll("<br />"," ");
+
+        // Create review object, assigning ID and real class
+        MovieReview review = new MovieReview(ID, text, realClass);
+        //inFile.close();
+        // Update ID
+        ID++;
+        // Classify review
+        classifyReview(review);
+
+        return review;
     }
 
-    
     /**
      * Deletes a review from the database, given its id.
      * @param id The id value of the review.
      */
+    @Override
     public void deleteReview(int id) {
-    	
+
+        if (!database.containsKey(id)) {
+            // Review with given ID does not exist
+            SentimentAnalysisApp.outputArea.append("ID " + id + " does not exist.");
+        } else {
             database.remove(id);
+            SentimentAnalysisApp.outputArea.append("Review with ID " + id + " deleted.");
+        }
+        return;
     }
-    
-    
+
     /**
      * Loads review database.
      */
-	public void loadSerialDB() {
-		File data = new File(DATA_FILE_NAME);
-		if(data.exists() == false)
-		{
-		try {
-		 FileOutputStream fileOut = new FileOutputStream(DATA_FILE_NAME);
-         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-         out.writeObject(database);
-         out.close();
-         fileOut.close();
-		}
-		catch(IOException e) {
-			System.out.println(e.getMessage());
-		}
-		}
-		try
-		{
-			FileInputStream fileIn = new FileInputStream(DATA_FILE_NAME);
-			ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-			database = (HashMap) objectIn.readObject();
-			objectIn.close();
-			fileIn.close();
-		}
-		catch(IOException e) {
-			System.out.println(e.getMessage());
-		}
-		catch(ClassNotFoundException i) {
-			System.out.println(i.getMessage());
-		}
-	}
-	
-	
+    @Override
+    @SuppressWarnings("unchecked")
+    public void loadSerialDB() {
+        SentimentAnalysisApp.outputArea.append("Reading database...");
+        // serialize the database
+        InputStream file = null;
+        InputStream buffer = null;
+        ObjectInput input = null;
+        try {
+            file = new FileInputStream(DATA_FILE_NAME);
+            buffer = new BufferedInputStream(file);
+            input = new ObjectInputStream(buffer);
+
+            database = (Map<Integer, MovieReview>)input.readObject();
+            SentimentAnalysisApp.outputArea.append(database.size() + " entry(s) loaded.\n");
+
+            // get the maximum ID
+            for (Map.Entry<Integer, MovieReview> entry : database.entrySet()){
+                if (entry.getKey() > ID) {
+                    ID = entry.getKey();
+                }
+            }
+            ID = ID + 1;
+
+            input.close();
+        } catch (IOException e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+        } catch (ClassCastException e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+        } finally {
+            close(file);
+        }
+        SentimentAnalysisApp.outputArea.append("Done.\n");
+    }
+
     /**
      * Searches the review database by id.
      * @param id The id to search for.
-     * @return The review that matches the given id or null if the id does not 
+     * @return The review that matches the given id or null if the id does not
      * exist in the database.
      */
+    @Override
     public MovieReview searchById(int id) {
-        
-    	return database.get(id);
+        if (database.containsKey(id)) {
+            return database.get(id);
+        }
+        return null;
     }
-    
-    
+
     /**
      * Searches the review database for reviews matching a given substring.
      * @param substring The substring to search for.
      * @return A list of review objects matching the search criterion.
      */
+    @Override
     public List<MovieReview> searchBySubstring(String substring) {
-        //for menu option 3
-    	List<MovieReview> reviewList = new ArrayList<MovieReview>();
-        for(MovieReview review : database.values()) {
-            if (review.getText().indexOf(substring) >= 0)
-            	reviewList.add(review);
-        }
-        return reviewList;
-    }
+        List<MovieReview> tempList = new ArrayList<MovieReview>();
 
-	
-    /**
-     * Calls classifyReview(MovieReview review) to handle multiple reviews
-     * at once.
-     */
-    public void classifyReviews() {
-    	for (MovieReview mr : database.values()) {
-            classifyReview(mr);
+        for (Map.Entry<Integer, MovieReview> entry : database.entrySet()){
+            if (entry.getValue().getText().contains(substring)) {
+                tempList.add(entry.getValue());
+            }
         }
-    }
-    
-    
-    /**
-     * Reports the overall classification accuracy if the real class is known.
-     * @param realClass The real class entered by the user.
-     */
-    public void reportAccuracy(int realClass) {
-    	
-    	if(realClass == 2) return;
 
-        int total = database.values().size();
-        int count = 0;
-        for (MovieReview mr : database.values()) {
-            int review = mr.getPredictedPolarity();
-            if (review == realClass)
-                count++;
+        if (tempList.size()!=0) {
+            return tempList;
+        } else {
+            // No review has given substring
+            return null;
         }
-        double accuracy = (count * 100.0 / total);
-        System.out.format("Accuracy of classification : %.2f", accuracy);
-        System.out.println("%");
-    }
 
+    }
 }
